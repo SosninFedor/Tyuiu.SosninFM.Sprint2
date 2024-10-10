db401caba5cbc485ad95ca8ebd72270c18f3e85f
федор соснин <sosninfedor@gmail.com>
2024-10-11 02:33:49 +0500

    Task333

---
+++ Tyuiu.SosninFM.Sprint2.Task3.V23/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Tyuiu.SosninFM.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.SosninFM.Sprint2.Task3.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
