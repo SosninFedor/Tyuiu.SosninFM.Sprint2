08a9e6640b99a253683d9e7319b1e4a0639d48a7
федор соснин <sosninfedor@gmail.com>
2024-10-10 23:38:37 +0500

    Task2

--- Tyuiu.SosninFM.Sprint2.Task2.V9/Program.cs
+++ Tyuiu.SosninFM.Sprint2.Task2.V9/Program.cs
@@ -7,6 +7,45 @@ namespace Tyuiu.SosninFM.Sprint2.Task2.V9
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Соснин Ф. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнил: Соснин Фёдор Михайлович | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ведите значение переменной Х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ведите значение переменной У: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+
+            }
+            else
+            {
+                Console.WriteLine("Точка находится вне заштрихованной области");
+            }
+
 
         }
     }
