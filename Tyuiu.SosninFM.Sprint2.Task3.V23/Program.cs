22b9ec8e7d08e9dd97827fed9654de8c005ae429
федор соснин <sosninfedor@gmail.com>
2024-10-11 17:16:51 +0500

    Task3

--- Tyuiu.SosninFM.Sprint2.Task3.V23/Program.cs
+++ Tyuiu.SosninFM.Sprint2.Task3.V23/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.Win32.SafeHandles;
 using Tyuiu.SosninFM.Sprint2.Task3.V23.Lib;
 
 namespace Tyuiu.SosninFM.Sprint2.Task3.V23
@@ -7,7 +8,36 @@ namespace Tyuiu.SosninFM.Sprint2.Task3.V23
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Соснин Ф. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнил: Соснин Фёдор Михайлович | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение Функции = " + res);
+
+            Console.ReadKey();
+
+
         }
     }
 }
