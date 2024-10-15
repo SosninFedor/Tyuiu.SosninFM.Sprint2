d689c234a6c3870bf7e3a17c3f1a6256b169ad5f
федор соснин <sosninfedor@gmail.com>
2024-10-16 00:01:18 +0500

    Task6

---
+++ Tyuiu.SosninFM.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Tyuiu.SosninFM.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.SosninFM.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Соснин Ф. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнил: Соснин Фёдор Михайлович | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+            int res;
+            if ((numMonth > 12) || (numMonth < 1))
+            {
+                res = 0;
+            }
+            else
+            {
+                res = ds.FindMonthDaysCount(numMonth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Количество дней в месяце: {res}");
+            Console.ReadKey();
+        }
+    }
+}
